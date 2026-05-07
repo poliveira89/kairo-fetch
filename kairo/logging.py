6f5ad31f583e56a0a686475077bbe07ea7d87cab
poliveira89 <poliveira89@users.noreply.github.com>
2026-05-07 11:32:37 +0100

    chore(logging): remove unnecessary comments

--- kairo/logging.py
+++ kairo/logging.py
@@ -4,10 +4,8 @@ import sys
 
 from loguru import logger
 
-# Remove default handler
 logger.remove()
 
-# Configure custom logger
 logger.add(
     sys.stderr,
     format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {message}",
